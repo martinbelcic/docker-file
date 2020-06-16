cde45c054449ffcbc6d6f4db672186f9fc78713c
Tinchota <martin.belcic@gmail.com>
2020-06-16 20:37:22 -0300

    Termina2

--- command.sh
+++ command.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 sudo docker build -t tickets - < Dockerfile
-sudo docker run -p 12345:12345 tickets
+sudo docker run -p 12345:12345 --mount source=my-vol,target=/usr/local/Tickets tickets
+
 
